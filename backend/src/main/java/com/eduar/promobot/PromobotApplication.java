c240453e85e7d63168dc8c5d8831c4be948d124a
eduardo ciudad figueredo <eduardociudadfigueredo@gmail.com>
2026-08-08 12:37:53 -0300

    feat: create spring initialyzer

---
+++ backend/src/main/java/com/eduar/promobot/PromobotApplication.java
@@ -0,0 +1,13 @@
+package com.eduar.promobot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PromobotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PromobotApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/eduar/promobot/PromobotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eduar.promobot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PromobotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
