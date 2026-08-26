2a3b69cb23658a6a0177675aa0093788c21a2f0b
eduardo ciudad figueredo <eduardociudadfigueredo@gmail.com>
2026-08-26 16:42:40 -0300

    feat: create DestinatarioIndisponivelException

---
+++ backend/src/main/java/com/eduar/promobot/domain/exception/DestinatarioIndisponivelException.java
@@ -0,0 +1,8 @@
+package com.eduar.promobot.domain.exception;
+
+public class DestinatarioIndisponivelException extends RuntimeException {
+
+    public DestinatarioIndisponivelException(String message) {
+        super(message);
+    }
+}
