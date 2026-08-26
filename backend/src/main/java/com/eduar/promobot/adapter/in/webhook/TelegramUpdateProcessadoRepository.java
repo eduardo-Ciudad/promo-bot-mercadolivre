8393e91cf8e7326aade8c66d97e57deaa4d79e49
eduardo ciudad figueredo <eduardociudadfigueredo@gmail.com>
2026-08-26 16:43:06 -0300

    feat: create TelegramUpdateProcessadoRepository

---
+++ backend/src/main/java/com/eduar/promobot/adapter/in/webhook/TelegramUpdateProcessadoRepository.java
@@ -0,0 +1,6 @@
+package com.eduar.promobot.adapter.in.webhook;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TelegramUpdateProcessadoRepository extends JpaRepository<TelegramUpdateProcessado, Long> {
+}
