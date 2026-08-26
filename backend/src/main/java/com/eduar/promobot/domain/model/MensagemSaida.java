40287447308ded412d24bee004c9228f77567d6e
eduardo ciudad figueredo <eduardociudadfigueredo@gmail.com>
2026-08-26 16:42:38 -0300

    feat: create MensagemSaida

---
+++ backend/src/main/java/com/eduar/promobot/domain/model/MensagemSaida.java
@@ -0,0 +1,15 @@
+package com.eduar.promobot.domain.model;
+
+import java.net.URI;
+import java.util.UUID;
+
+public record MensagemSaida(
+        UUID mensagemId,
+        CanalDistribuicao canal,
+        String destinoExterno,
+        String texto,
+        URI imagemUrl,
+        URI link,
+        String titulo
+) {
+}
