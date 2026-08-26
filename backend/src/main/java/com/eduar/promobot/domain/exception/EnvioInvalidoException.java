8dd344276595bfc6cc53386190c954aad4573956
eduardo ciudad figueredo <eduardociudadfigueredo@gmail.com>
2026-08-26 16:42:40 -0300

    feat: create EnvioInvalidoException

---
+++ backend/src/main/java/com/eduar/promobot/domain/exception/EnvioInvalidoException.java
@@ -0,0 +1,8 @@
+package com.eduar.promobot.domain.exception;
+
+public class EnvioInvalidoException extends RuntimeException {
+
+    public EnvioInvalidoException(String message) {
+        super(message);
+    }
+}
