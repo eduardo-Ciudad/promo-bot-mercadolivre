98c1c3b1f854e10643ebfa0bc27b6763c56e9bca
eduardo ciudad figueredo <eduardociudadfigueredo@gmail.com>
2026-08-26 16:42:41 -0300

    feat: create EnvioFalhouException

---
+++ backend/src/main/java/com/eduar/promobot/domain/exception/EnvioFalhouException.java
@@ -0,0 +1,8 @@
+package com.eduar.promobot.domain.exception;
+
+public class EnvioFalhouException extends RuntimeException {
+
+    public EnvioFalhouException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
